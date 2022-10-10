53176b0ab882887d97665ca4b0f7e3f8980f5a8d
hackingyseguridad <antonio_taboada@hotmail.es>
2022-10-10 10:15:41 +0200

    Update modificadohoy.sh

--- modificadohoy.sh
+++ modificadohoy.sh
@@ -1,7 +1,7 @@
+#!/bin/bash
+# (c) hacking y seguridad .com 2022
+# lista los ficheros en local modificados recientemente
 find / -type f -mtime -1 -print
-
 #find / -type f -mtime -1 -exec ls -ld {} \;
-
 #find . -type d -exec ls -ld {} \;
- 
 #ls -alR -lt
