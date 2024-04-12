21dc6931623eb75f8e3613a6ca4ca2c399ccee08
hackingyseguridad <antonio_taboada@hotmail.es>
2024-04-12 08:08:34 +0200

    Update modificadoahora.sh

--- modificadoahora.sh
+++ modificadoahora.sh
@@ -1,5 +1,5 @@
-
-
-
+#!/bin/bash
+# (R) hacking y seguridad .com 2024
+# lista los ficheros en carpeta local modificados recientemente
 
 find /home/antonio/scan -type f -printf "%T+ %f\n" | sort | tail
