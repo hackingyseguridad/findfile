9534674a9c195a78e5e1d871ad060f69941fda7f
hackingyseguridad <antonio_taboada@hotmail.es>
2026-05-18 12:41:09 +0200

    Create busca_bbdd.sh

---
+++ busca_bbdd.sh
@@ -0,0 +1,16 @@
+
+
+
+find / -type f \( \
+  -name "*.db" -o \
+  -name "*.sqlite" -o \
+  -name "*.sqlite3" -o \
+  -name "*.sql" -o \
+  -name "*.sql.gz" -o \
+  -name "*.sql.bz2" -o \
+  -name "*.dump" -o \
+  -name "*.ibd" -o \
+  -name "*.frm" -o \
+  -name "*.mdb" -o \
+  -name "*.accdb" \
+\) 2>/dev/null | head -50
