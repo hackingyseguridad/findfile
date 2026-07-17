349a5919fd6860ea05095fb4bbdf2cad1ffe6306
hackingyseguridad <antonio_taboada@hotmail.es>
2026-07-17 15:27:28 +0200

    Update depura.sh

--- depura.sh
+++ depura.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-# Script para deputar el fichero resultado.txt, extraer las IP IPv4  y eliminar duplicados
-# Script para IPv4
-# Lee del fichero resultado.txt la lista de IP, entrega el resultado en el fichero resultado2.txt
+# Script para depurar el fichero resultado.txt, extraer IP IPv4 e IPv6 y eliminar duplicados
+# Script para IPv4 + IPv6
+# Lee del fichero resultado.txt la lista de IP, entrega el resultado final en ip.txt
 # Uso: sh depura.sh
 # (C) Antonio Taboada - www.hackingyseguridad.com 2023
+
 echo
 echo ".."
-# Limpia de caracteres especiales el fichero de entrada ip.txt con direcciones IP IPv4
+# Extrae direcciones IPv4 del fichero de entrada resultado.txt
 grep -Eo "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" resultado.txt > resultado1.txt
 echo "..."
-# Elimina IP duplicadas y genera resultado3.txt
+# Extrae direcciones IPv6 del fichero de entrada resultado.txt
+grep -Eo "([0-9A-Fa-f]{1,4}:){7}[0-9A-Fa-f]{1,4}|([0-9A-Fa-f]{1,4}:){1,7}:|([0-9A-Fa-f]{1,4}:){1,6}:[0-9A-Fa-f]{1,4}|([0-9A-Fa-f]{1,4}:){1,5}(:[0-9A-Fa-f]{1,4}){1,2}|([0-9A-Fa-f]{1,4}:){1,4}(:[0-9A-Fa-f]{1,4}){1,3}|([0-9A-Fa-f]{1,4}:){1,3}(:[0-9A-Fa-f]{1,4}){1,4}|([0-9A-Fa-f]{1,4}:){1,2}(:[0-9A-Fa-f]{1,4}){1,5}|[0-9A-Fa-f]{1,4}:((:[0-9A-Fa-f]{1,4}){1,6})|:((:[0-9A-Fa-f]{1,4}){1,7}|:)" resultado.txt >> resultado1.txt
+echo "...."
+# Elimina IP duplicadas y genera resultado2.txt
 awk '!visited[$0]++' "resultado1.txt" > resultado2.txt
 echo "... Ok"
 cp resultado2.txt ip.txt
